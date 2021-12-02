0b0c77ccbda06413ee3a3f42055d7f12a812568a
Jesse Luehrs <doy@tozt.net>
2021-12-01 22:17:05 -0500

    2021 day 1

---
+++ src/2021/1/mod.rs
@@ -0,0 +1,22 @@
+pub fn part1() -> anyhow::Result<i64> {
+    Ok(data_ints!()?
+        .windows(2)
+        .map(|a| a[1] - a[0])
+        .filter(|x| *x > 0)
+        .count()
+        .try_into()
+        .unwrap())
+}
+
+pub fn part2() -> anyhow::Result<i64> {
+    Ok(data_ints!()?
+        .windows(3)
+        .map(|a| a[0] + a[1] + a[2])
+        .collect::<Vec<_>>()
+        .windows(2)
+        .map(|a| a[1] - a[0])
+        .filter(|x| *x > 0)
+        .count()
+        .try_into()
+        .unwrap())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@
 
 #[macro_use]
 mod util;
+
 #[path = "2020/mod.rs"]
 mod year2020;
+#[path = "2021/mod.rs"]
+mod year2021;
 
 #[derive(Debug, structopt::StructOpt)]
 #[structopt(about = "Advent of Code")]
 enum Opt {
     #[structopt(name = "2020")]
     Year2020 { day: u8, puzzle: u8 },
+    #[structopt(name = "2021")]
+    Year2021 { day: u8, puzzle: u8 },
 }
 
 #[paw::main]
 fn main(opt: Opt) {
     let res = match opt {
         Opt::Year2020 { day, puzzle } => crate::year2020::run(day, puzzle),
+        Opt::Year2021 { day, puzzle } => crate::year2021::run(day, puzzle),
     };
     match res {
         Ok(answer) => {
