9b65015635fca202136cbd042037dd28bfd549d7
Jesse Luehrs <doy@tozt.net>
2021-12-07 00:10:55 -0500

    day 7

---
+++ src/2021/7/mod.rs
@@ -0,0 +1,38 @@
+pub fn part1() -> anyhow::Result<i64> {
+    let crabs: Vec<i64> = data_lines!()?
+        .next()
+        .unwrap()?
+        .split(',')
+        .map(|s| s.parse())
+        .collect::<Result<_, _>>()?;
+
+    Ok((0..=crabs.iter().copied().max().unwrap())
+        .map(|start| {
+            crabs.iter().copied().map(|crab| (crab - start).abs()).sum()
+        })
+        .min()
+        .unwrap())
+}
+
+pub fn part2() -> anyhow::Result<i64> {
+    let crabs: Vec<i64> = data_lines!()?
+        .next()
+        .unwrap()?
+        .split(',')
+        .map(|s| s.parse())
+        .collect::<Result<_, _>>()?;
+
+    Ok((0..=crabs.iter().copied().max().unwrap())
+        .map(|start| {
+            crabs
+                .iter()
+                .copied()
+                .map(|crab| {
+                    let diff = (crab - start).abs();
+                    diff * (diff + 1) / 2
+                })
+                .sum()
+        })
+        .min()
+        .unwrap())
+}
