76e71bc89d995078c914b63f909d7ee346caf5d9
Jesse Luehrs <doy@tozt.net>
2022-12-02 00:41:10 -0500

    day 2

--- src/2022/mod.rs
+++ src/2022/mod.rs
@@ -2,6 +2,8 @@ use crate::prelude::*;
 
 #[path = "1/mod.rs"]
 mod day1;
+#[path = "2/mod.rs"]
+mod day2;
 // NEXT MOD
 
 pub fn run(day: u8, puzzle: u8) -> Result<i64> {
@@ -9,6 +11,8 @@ pub fn run(day: u8, puzzle: u8) -> Result<i64> {
     match (day, puzzle) {
         (1, 1) => day1::part1(day1::parse(parse::data(2022, 1)?)?),
         (1, 2) => day1::part2(day1::parse(parse::data(2022, 1)?)?),
+        (2, 1) => day2::part1(day2::parse(parse::data(2022, 2)?)?),
+        (2, 2) => day2::part2(day2::parse(parse::data(2022, 2)?)?),
         // NEXT PART
         _ => Err(anyhow!("unknown puzzle {}-{}", day, puzzle)),
     }
