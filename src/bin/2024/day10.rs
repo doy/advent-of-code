63304b8d134c9c94b4741f92cf0dbca3d2056dc9
Jesse Luehrs <doy@tozt.net>
2024-12-10 00:46:40 -0500

    simplify

--- src/bin/2024/day10.rs
+++ src/bin/2024/day10.rs
@@ -1,4 +1,4 @@
-use advent_of_code::{graph::Graph, prelude::*};
+use advent_of_code::prelude::*;
 
 pub struct Map(Grid<u8>);
 
@@ -35,41 +35,33 @@ pub fn parse(fh: File) -> Result<Map> {
 }
 
 pub fn part1(map: Map) -> Result<i64> {
-    let trailheads: Vec<Pos> = map
+    Ok(map
         .0
         .indexed_cells()
         .filter_map(|(pos, c)| (*c == 0).then_some(pos))
-        .collect();
-    let peaks: Vec<Pos> = map
-        .0
-        .indexed_cells()
-        .filter_map(|(pos, c)| (*c == 9).then_some(pos))
-        .collect();
-    let mut total = 0;
-    for trailhead in trailheads {
-        let mut score = 0;
-        let paths = map.dijkstra_full(trailhead);
-        for peak in &peaks {
-            if matches!(paths.get(peak), Some((_, 9))) {
-                score += 1;
-            }
-        }
-        total += score;
-    }
-    Ok(total)
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .map(|trailhead| {
+            i64::try_from(
+                map.dijkstra_full(trailhead)
+                    .values()
+                    .filter(|(_, len)| *len == 9)
+                    .count(),
+            )
+            .unwrap()
+        })
+        .sum())
 }
 
 pub fn part2(map: Map) -> Result<i64> {
-    let peaks: Vec<Pos> = map
+    Ok(map
         .0
         .indexed_cells()
         .filter_map(|(pos, c)| (*c == 9).then_some(pos))
-        .collect();
-    let mut total = 0;
-    for peak in peaks {
-        total += map.rating(peak);
-    }
-    Ok(total)
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .map(|peak| map.rating(peak))
+        .sum())
 }
 
 #[test]
