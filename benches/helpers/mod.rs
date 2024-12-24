ffde87fd6ac32bbfe7305511148805102eaef770
Jesse Luehrs <doy@tozt.net>
2024-12-23 23:31:04 -0500

    rework benchmarks a bit

--- benches/helpers/mod.rs
+++ benches/helpers/mod.rs
@@ -2,36 +2,16 @@ macro_rules! day {
     ($year:expr, $day:expr, $mod:ident) => {
         paste::paste! {
             #[allow(unused_must_use)]
-            fn [<bench_ $year $mod _parse>](c: &mut criterion::Criterion) {
-                c.bench_function(
-                    &format!("{} day {} parse", $year, $day),
-                    |b| {
-                        b.iter_batched(
-                            || advent_of_code::parse::data($year, $day).unwrap(),
-                            |data| $mod::parse(criterion::black_box(data)),
-                            criterion::BatchSize::PerIteration,
-                        )
-                    }
-                );
-            }
-        }
-
-        paste::paste! {
-            #[allow(unused_must_use)]
             fn [<bench_ $year $mod _part1>](c: &mut criterion::Criterion) {
                 c.bench_function(
                     &format!("{} day {} part 1", $year, $day),
                     |b| {
-                        b.iter_batched(
-                            || {
-                                $mod::parse(
-                                    advent_of_code::parse::data($year, $day)
-                                        .unwrap(),
-                                )
-                                .unwrap()
-                            },
-                            |data| $mod::part1(criterion::black_box(data)),
-                            criterion::BatchSize::PerIteration,
+                        b.iter(
+                            || $mod::part1(
+                                $mod::parse(advent_of_code::parse::data($year, $day).unwrap())
+                                    .unwrap(),
+                            )
+                            .unwrap()
                         )
                     }
                 );
@@ -44,16 +24,12 @@ macro_rules! day {
                 c.bench_function(
                     &format!("{} day {} part 2", $year, $day),
                     |b| {
-                        b.iter_batched(
-                            || {
-                                $mod::parse(
-                                    advent_of_code::parse::data($year, $day)
-                                        .unwrap(),
-                                )
-                                .unwrap()
-                            },
-                            |data| $mod::part2(criterion::black_box(data)),
-                            criterion::BatchSize::PerIteration,
+                        b.iter(
+                            || $mod::part2(
+                                $mod::parse(advent_of_code::parse::data($year, $day).unwrap())
+                                    .unwrap(),
+                            )
+                            .unwrap()
                         )
                     }
                 );
@@ -63,7 +39,6 @@ macro_rules! day {
         paste::paste! {
             criterion::criterion_group!(
                 [<bench_ $year $mod>],
-                [<bench_ $year $mod _parse>],
                 [<bench_ $year $mod _part1>],
                 [<bench_ $year $mod _part2>],
             );
