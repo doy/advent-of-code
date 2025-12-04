d3476bc1ea0734996b2454024579355fe2195769
Jesse Luehrs <doy@tozt.net>
2025-12-04 00:15:59 -0500

    day 4

--- benches/2025.rs
+++ benches/2025.rs
@@ -7,11 +7,14 @@ mod day1;
 mod day2;
 #[path = "../src/bin/2025/day3.rs"]
 mod day3;
+#[path = "../src/bin/2025/day4.rs"]
+mod day4;
 // NEXT MOD
 
 day!(2025, 1, day1);
 day!(2025, 2, day2);
 day!(2025, 3, day3);
+day!(2025, 4, day4);
 // NEXT DAY
 
 fn bench_2024(c: &mut criterion::Criterion) {
@@ -20,6 +23,7 @@ fn bench_2024(c: &mut criterion::Criterion) {
             day_combined!(2025, 1, day1);
             day_combined!(2025, 2, day2);
             day_combined!(2025, 3, day3);
+            day_combined!(2025, 4, day4);
             // NEXT DAY COMBINED
         })
     });
@@ -31,5 +35,6 @@ criterion::criterion_main!(
     bench_2025day1,
     bench_2025day2,
     bench_2025day3,
+    bench_2025day4,
     // NEXT GROUP
 );
---
+++ src/bin/2025/day4.rs
@@ -0,0 +1,59 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use advent_of_code::prelude::*;
+
+pub fn parse(fh: File) -> Result<Grid<bool>> {
+    Ok(parse::bool_grid(parse::raw_lines(fh), b'@', b'.'))
+}
+
+pub fn part1(map: Grid<bool>) -> Result<i64> {
+    Ok(map
+        .indexed_cells()
+        .map(|(pos, cell)| {
+            *cell
+                && map.adjacent(pos, true).filter(|pos| map[*pos]).count() < 4
+        })
+        .filter(|b| *b)
+        .count()
+        .try_into()
+        .unwrap())
+}
+
+pub fn part2(mut map: Grid<bool>) -> Result<i64> {
+    let mut removed = 0;
+    loop {
+        let to_remove: Vec<_> = map
+            .indexed_cells()
+            .filter_map(|(pos, cell)| {
+                (*cell
+                    && map
+                        .adjacent(pos, true)
+                        .filter(|pos| map[*pos])
+                        .count()
+                        < 4)
+                .then_some(pos)
+            })
+            .collect();
+        if to_remove.is_empty() {
+            return Ok(removed);
+        } else {
+            removed += i64::try_from(to_remove.len()).unwrap();
+            for pos in to_remove {
+                map[pos] = false;
+            }
+        }
+    }
+}
+
+#[test]
+fn test() {
+    assert_eq!(
+        part1(parse(parse::data(2025, 4).unwrap()).unwrap()).unwrap(),
+        1370
+    );
+    assert_eq!(
+        part2(parse(parse::data(2025, 4).unwrap()).unwrap()).unwrap(),
+        8437
+    );
+}
